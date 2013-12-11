c96c539f68a3ca057398a14fb03ea37c030a2da8
Morten Nielsen <no@spam.thx>
2013-12-10 23:34:00 -0800

    Replaced PixelFormat with built-in BitmapPixelFormat + Replaced SavePpm with CreateImageSourceAsync()

--- src/AntiGrainRT/RenderingBuffer.cpp
+++ src/AntiGrainRT/RenderingBuffer.cpp
@@ -1,106 +1,132 @@
 ﻿#include "pch.h"
 #include "RenderingBuffer.h"
 
+using namespace Platform;
+using namespace Windows::Foundation;
 using namespace Windows::Storage::Streams;
 using namespace concurrency;
 using namespace AntiGrainRT;
-using namespace Platform;
+using namespace concurrency;
+#if (WINAPI_FAMILY==WINAPI_FAMILY_APP)
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::Graphics::Imaging;
+using namespace Windows::UI::Xaml::Media::Imaging;
+#endif
 
 void RenderingBuffer::SetPixel(unsigned int x, unsigned int y, Windows::UI::Color color)
 {
 	//	// Get the pointer to the beginning of the i-th row (Y-coordinate)
 	//	// and shift it to the i-th position, that is, X-coordinate.
-	unsigned char* ptr = m_rbuf.row_ptr(y) + x * 3;
-  if (m_format == AntiGrainRT::PixelFormat::Rgb24) 
+	unsigned char* ptr = m_rbuf.row_ptr(y) + x * 4;
+  if (m_format == BitmapPixelFormat::Rgba8) 
   {
     *ptr++ = color.R; // R
     *ptr++ = color.G; // G
     *ptr++ = color.B;  // B
+    *ptr++ = color.A;  // B
   }
-  else if (m_format == AntiGrainRT::PixelFormat::Bgr24)
+  else if (m_format == BitmapPixelFormat::Bgra8)
   {
     *ptr++ = color.B; // R
     *ptr++ = color.G; // G
     *ptr++ = color.R;  // B
+    *ptr++ = color.A;  // B
   }
 }
 
 Windows::UI::Color RenderingBuffer::GetPixel(unsigned int x, unsigned int y)
 {
-  unsigned char* ptr = m_rbuf.row_ptr(y) + x * 3;
+  unsigned char* ptr = m_rbuf.row_ptr(y) + x * 4;
   Windows::UI::Color color;
-  if (m_format == AntiGrainRT::PixelFormat::Rgb24)
+  if (m_format == BitmapPixelFormat::Rgba8)
   {
     color.R = *ptr++;
     color.G = *ptr++;
     color.B = *ptr++;
+    color.A = *ptr++;
   }
-  else if (m_format == AntiGrainRT::PixelFormat::Bgr24)
+  else if (m_format == BitmapPixelFormat::Bgra8)
   {
     color.B = *ptr++;
     color.G = *ptr++;
     color.R = *ptr++;
+    color.A = *ptr++;
   }
-  color.A = 255;
   return color;
 }
 
-RenderingBuffer::RenderingBuffer(unsigned int width, unsigned int height, AntiGrainRT::PixelFormat format)
+RenderingBuffer::RenderingBuffer(unsigned int width, unsigned int height, BitmapPixelFormat format)
 : m_width(width), m_height(height), m_format(format)
 {
   
   
-	m_buffer = ref new ::Platform::Array<uint8>(width * height * 3);
+	m_buffer = ref new ::Platform::Array<uint8>(width * height * 4);
 
-	memset(m_buffer->Data, 255, width * height * 3);
+	memset(m_buffer->Data, 255, width * height * 4);
 
 	agg::rendering_buffer rbuf(m_buffer->Data,
 		width,
 		height,
-		width * 3);
+		width * 4);
 	m_rbuf = rbuf;
-
-	//for (i = 0; i < rbuf.height(); ++i)
-	//{
-	//	unsigned char* p = rbuf.row_ptr(i);
-	//	*p++ = 0; *p++ = 0; *p++ = 0;
-	//	p += (rbuf.width() - 2) * 3;
-	//	*p++ = 0; *p++ = 0; *p++ = 0;
-	//}
-	//memset(rbuf.row_ptr(0), 0, rbuf.width() * 3);
-	//memset(rbuf.row_ptr(rbuf.height() - 1), 0, rbuf.width() * 3);
-
-
-	//draw_black_frame(rbuf);
 }
 
-Windows::Foundation::IAsyncAction^ RenderingBuffer::SavePpm(Windows::Storage::IStorageFile^ file)
+//Windows::Foundation::IAsyncAction^ RenderingBuffer::SavePpmAsync(Windows::Storage::IStorageFile^ file)
+//{
+//	auto result_async_operation =
+//		create_async([=](void)
+//	{
+//    auto stream = create_task(file->OpenAsync(Windows::Storage::FileAccessMode::ReadWrite)).get();
+//    auto outstream = stream->GetOutputStreamAt(0);
+//    DataWriter^ writer = ref new DataWriter(outstream);
+//    writer->WriteString("P6 " + m_width + " " + m_height + " 255 ");
+//    writer->WriteBytes(m_buffer);
+//    writer->StoreAsync(); //TODO: This is not awaited
+//	});
+//	return result_async_operation;
+//}
+
+#if (WINAPI_FAMILY==WINAPI_FAMILY_APP)
+
+IAsyncOperation<ImageSource^>^ RenderingBuffer::CreateImageSourceAsync()
 {
-	//auto path = Windows::Storage::ApplicationData::Current->TemporaryFolder->Path + "\\out.ppm";
-	//FILE* fd;
-	//auto err = fopen_s(&fd, file->Path->Data(), "wb");
-	//if (fd)
-	//{
-	//	fprintf(fd, "P6 %d %d 255 ", m_width, m_height);
-	//	fwrite(m_buffer->Data, 1, m_buffer->Length, fd);
-	//	fclose(fd);
-	//}
-	//auto header = ref new Platform::String("P6 " + m_width + " " + m_height + " 255 ");
-	auto buffer = m_buffer;
-	//auto op = create_async([file, header](void) { Windows::Storage::FileIO::WriteTextAsync(file, header); })
-    //                 .then([file, buffer]() { return Windows::Storage::FileIO::WriteBytesAsync(file, buffer); });
-	//return op;
-	auto result_async_operation =
-		create_async([file, buffer](void)
-	{
-	//	auto stream = ref new InMemoryRandomAccessStream();
-	//	auto openTask = create_task(file->OpenAsync(Windows::Storage::FileAccessMode::ReadWrite))
-	//		.then([data](IRandomAccessStream^ stream)
-	//	{
-	//		stream->Seek(0);
-	//		stream->WriteAsync()
-	//	});
-	//	return openTask;
-	});
-	return result_async_operation;
+  auto buffer = m_buffer;
+  // we need to return an IAsyncOperation, so use create_async to create that code in a lambda
+  auto result_async_operation =
+    create_async([=](void)
+  {
+    // This will be the result bitmap, we create it here so it gets captured in lambda later on
+    auto bitmap = ref new BitmapImage();
+    auto stream = ref new InMemoryRandomAccessStream();
+
+    // Note: used chained thens to ensure we can access the original calling context (the ui threadtypicall) at the end of the async sequence
+
+    // #1 create an encoder referencing the the empty stream 
+    auto task = create_task(BitmapEncoder::CreateAsync(BitmapEncoder::PngEncoderId, stream))
+
+      .then([=](BitmapEncoder^ encoder)
+    {
+      // #2 render pixel data into a byte array on a background thread
+      Array<unsigned char>^ pixels = buffer;
+      encoder->SetPixelData(BitmapPixelFormat::Bgra8, BitmapAlphaMode::Straight, m_width, m_height, 96, 96, pixels);
+      return encoder->FlushAsync();
+    }, task_continuation_context::use_arbitrary()) // run the above code on the thread pool (encoder are MTA objects so this is ok)
+
+      .then([stream, bitmap](void)
+    {
+      // #3 push the stream into the result bitmap, do this on the ui thread
+      stream->Seek(0);
+      return bitmap->SetSourceAsync(stream);
+    }) /* don't use thread pool - use ui (original calling context)*/
+
+      .then([bitmap](void)
+    {
+      // #4 wait for SetSourceAsync to finish, then return the bitmap as an image source
+      auto image_source = static_cast<Windows::UI::Xaml::Media::ImageSource^>(bitmap);
+      return image_source;
+    });
+    return task;
+  });
+  return result_async_operation;
 }
+#endif
--- src/AntiGrainRT/RenderingBuffer.h
+++ src/AntiGrainRT/RenderingBuffer.h
@@ -1,24 +1,25 @@
 ﻿#pragma once
 
-#include "PixelFormat.h"
-
 namespace AntiGrainRT
 {
   public ref class RenderingBuffer sealed
   {
     public:
-    RenderingBuffer(unsigned int width, unsigned int height, PixelFormat format);
-    void SetPixel(unsigned int x, unsigned int y, Windows::UI::Color color);
-    Windows::UI::Color GetPixel(unsigned int x, unsigned int y);
-    Windows::Foundation::IAsyncAction^ SavePpm(Windows::Storage::IStorageFile^ file);
-    property unsigned int PixelWidth { unsigned int get() { return m_width; } }
-    property unsigned int PixelHeight { unsigned int get() { return m_height; } }
-    property AntiGrainRT::PixelFormat PixelFormat { AntiGrainRT::PixelFormat get() { return m_format; } }
-  private:
-    agg::rendering_buffer m_rbuf;
-    Platform::Array<uint8>^ m_buffer;
-    unsigned int m_width;
-    unsigned int m_height;
-    AntiGrainRT::PixelFormat m_format;
+      RenderingBuffer(unsigned int width, unsigned int height, Windows::Graphics::Imaging::BitmapPixelFormat format);
+      void SetPixel(unsigned int x, unsigned int y, Windows::UI::Color color);
+      Windows::UI::Color GetPixel(unsigned int x, unsigned int y);
+      //Windows::Foundation::IAsyncAction^ SavePpmAsync(Windows::Storage::IStorageFile^ file);
+#if (WINAPI_FAMILY==WINAPI_FAMILY_APP)
+      Windows::Foundation::IAsyncOperation<Windows::UI::Xaml::Media::ImageSource^>^ CreateImageSourceAsync();
+#endif
+      property unsigned int PixelWidth { unsigned int get() { return m_width; } }
+      property unsigned int PixelHeight { unsigned int get() { return m_height; } }
+      property Windows::Graphics::Imaging::BitmapPixelFormat PixelFormat { Windows::Graphics::Imaging::BitmapPixelFormat get() { return m_format; } }
+    private:
+      agg::rendering_buffer m_rbuf;
+      Platform::Array<uint8>^ m_buffer;
+      unsigned int m_width;
+      unsigned int m_height;
+      Windows::Graphics::Imaging::BitmapPixelFormat m_format;
   };
 }
